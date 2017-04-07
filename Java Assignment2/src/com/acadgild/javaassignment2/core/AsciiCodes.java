51071d95ddc9ca9a2bb0e52e674c78cb77da45dc
prachi7792 <prachipednekar85@gmail.com>
2017-04-08 00:32:57 +0530

    Ascii Codes
    This Assignment says to print characters corresponding to Ascii Codes
    from 65 to 90  and output should be 65- A and 66- B

---
+++ Java Assignment2/src/com/acadgild/javaassignment2/core/AsciiCodes.java
@@ -0,0 +1,12 @@
+package com.acadgild.javaassignment2.core;
+
+public class AsciiCodes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    for (int i = 65; i <= 90; i++) {
+			System.out.println( i +"-" +(char)i);
+		}
+	}
+
+}
